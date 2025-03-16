6d2810e7594038faec3bfc1f82dac59be3517639
NDominus <ndominusdev@gmail.com>
2025-03-16 20:48:43 +0100

    added functionality to the site

--- find_skulls.py
+++ find_skulls.py
@@ -1,11 +1,11 @@
 import numpy as np
 from PIL import Image
 
-def gen_html_icon(coords, bg_shape):
+def gen_html_icon(i, coords, bg_shape):
     x,y = coords
     x = x / bg_shape[0] * 100
     y = y / bg_shape[1] * 100
-    return f'  <img class="icon" src="maps/skull_off.png" alt="Icon" style="top: {y:.4f}%; left: {x:.4f}%;">\n'
+    return f'<img class="icon-skull" style="top: {y:.4f}%; left: {x:.4f}%;">'
 
 paths = [
     "imgs/raw/skulls-downtown.png",
@@ -14,7 +14,7 @@ paths = [
     "imgs/raw/skulls-south_beach.png",
 ]
 
-out = ""
+out = "var html_maps = {"
 for path in paths:
     name = path.split('-')[1].split('.')[0]
 
@@ -23,10 +23,13 @@ for path in paths:
         mask = np.all(arr == [255, 0, 0], axis=-1)
         idxs = np.argwhere(mask)
 
-        out += '<div class="image-container">\n'
-        out += f'  <img src="maps/{name}.jpg" alt="">\n'
-        for idx in idxs: out += gen_html_icon(idx, img.size)
-        out += '</div>\n\n'
+        out += f'{name}: `<div class="image-container">'
+        out += f'<img id="map-bg" src="imgs/{name}.jpg" alt="">'
+        for i,idx in enumerate(idxs):
+            out += gen_html_icon(i, idx, img.size)
+        out += '</div>`,'
 
-with open("skulls.temp.html", "w") as file:
+out += "};"
+
+with open("skulls.temp.js", "w") as file:
     file.write(out)
---
+++ script.js
@@ -0,0 +1,89 @@
+var html_maps = { downtown: `<div class="image-container"><img id="map-bg" src="imgs/downtown.jpg" alt=""><img class="icon-skull" style="top: 57.2917%; left: 20.0000%;"><img class="icon-skull" style="top: 63.3333%; left: 23.1250%;"><img class="icon-skull" style="top: 73.1250%; left: 24.0625%;"><img class="icon-skull" style="top: 53.9583%; left: 26.5625%;"><img class="icon-skull" style="top: 60.0000%; left: 26.7188%;"><img class="icon-skull" style="top: 51.0417%; left: 30.3125%;"><img class="icon-skull" style="top: 77.0833%; left: 30.6250%;"><img class="icon-skull" style="top: 70.0000%; left: 31.0937%;"><img class="icon-skull" style="top: 67.2917%; left: 34.0625%;"><img class="icon-skull" style="top: 58.5417%; left: 35.0000%;"><img class="icon-skull" style="top: 50.6250%; left: 36.2500%;"><img class="icon-skull" style="top: 73.9583%; left: 38.1250%;"><img class="icon-skull" style="top: 46.4583%; left: 38.5938%;"><img class="icon-skull" style="top: 46.2500%; left: 43.1250%;"><img class="icon-skull" style="top: 52.5000%; left: 44.6875%;"><img class="icon-skull" style="top: 53.7500%; left: 48.7500%;"><img class="icon-skull" style="top: 45.4167%; left: 51.7188%;"><img class="icon-skull" style="top: 48.1250%; left: 57.3438%;"><img class="icon-skull" style="top: 36.6667%; left: 62.1875%;"><img class="icon-skull" style="top: 42.2917%; left: 66.0938%;"><img class="icon-skull" style="top: 40.4167%; left: 68.5938%;"><img class="icon-skull" style="top: 36.6667%; left: 71.7188%;"><img class="icon-skull" style="top: 21.0417%; left: 75.3125%;"></div>`, little_havana: `<div class="image-container"><img id="map-bg" src="imgs/little_havana.jpg" alt=""><img class="icon-skull" style="top: 56.6667%; left: 27.8125%;"><img class="icon-skull" style="top: 77.7083%; left: 31.8750%;"><img class="icon-skull" style="top: 60.4167%; left: 32.8125%;"><img class="icon-skull" style="top: 63.5417%; left: 42.3438%;"><img class="icon-skull" style="top: 17.5000%; left: 42.8125%;"><img class="icon-skull" style="top: 35.8333%; left: 46.4062%;"><img class="icon-skull" style="top: 46.6667%; left: 47.0312%;"><img class="icon-skull" style="top: 65.4167%; left: 47.1875%;"><img class="icon-skull" style="top: 31.6667%; left: 47.3438%;"><img class="icon-skull" style="top: 52.5000%; left: 47.5000%;"><img class="icon-skull" style="top: 74.7917%; left: 47.5000%;"><img class="icon-skull" style="top: 11.2500%; left: 50.6250%;"><img class="icon-skull" style="top: 20.8333%; left: 51.4062%;"><img class="icon-skull" style="top: 37.7083%; left: 53.1250%;"><img class="icon-skull" style="top: 55.8333%; left: 54.0625%;"><img class="icon-skull" style="top: 80.0000%; left: 54.8438%;"><img class="icon-skull" style="top: 26.0417%; left: 57.6562%;"><img class="icon-skull" style="top: 55.0000%; left: 63.1250%;"><img class="icon-skull" style="top: 28.5417%; left: 65.3125%;"><img class="icon-skull" style="top: 34.7917%; left: 65.6250%;"><img class="icon-skull" style="top: 83.9583%; left: 68.2812%;"></div>`, north_beach: `<div class="image-container"><img id="map-bg" src="imgs/north_beach.jpg" alt=""><img class="icon-skull" style="top: 49.5833%; left: 34.0625%;"><img class="icon-skull" style="top: 64.7917%; left: 35.1562%;"><img class="icon-skull" style="top: 58.1250%; left: 36.4062%;"><img class="icon-skull" style="top: 63.1250%; left: 37.9688%;"><img class="icon-skull" style="top: 72.9167%; left: 37.9688%;"><img class="icon-skull" style="top: 67.7083%; left: 40.3125%;"><img class="icon-skull" style="top: 65.2083%; left: 42.0312%;"><img class="icon-skull" style="top: 12.7083%; left: 45.0000%;"><img class="icon-skull" style="top: 34.3750%; left: 46.2500%;"><img class="icon-skull" style="top: 88.5417%; left: 47.1875%;"><img class="icon-skull" style="top: 59.1667%; left: 48.4375%;"><img class="icon-skull" style="top: 20.6250%; left: 48.5938%;"><img class="icon-skull" style="top: 67.7083%; left: 49.5312%;"><img class="icon-skull" style="top: 42.7083%; left: 52.5000%;"><img class="icon-skull" style="top: 73.5417%; left: 54.5312%;"><img class="icon-skull" style="top: 20.2083%; left: 55.1562%;"><img class="icon-skull" style="top: 59.3750%; left: 56.0938%;"><img class="icon-skull" style="top: 25.0000%; left: 56.2500%;"><img class="icon-skull" style="top: 74.3750%; left: 56.4062%;"><img class="icon-skull" style="top: 30.8333%; left: 57.9688%;"><img class="icon-skull" style="top: 50.6250%; left: 58.7500%;"><img class="icon-skull" style="top: 55.6250%; left: 59.5312%;"><img class="icon-skull" style="top: 21.8750%; left: 61.8750%;"><img class="icon-skull" style="top: 14.3750%; left: 62.5000%;"><img class="icon-skull" style="top: 56.8750%; left: 66.4062%;"><img class="icon-skull" style="top: 19.3750%; left: 67.9688%;"></div>`, south_beach: `<div class="image-container"><img id="map-bg" src="imgs/south_beach.jpg" alt=""><img class="icon-skull" style="top: 73.3333%; left: 35.1562%;"><img class="icon-skull" style="top: 40.6250%; left: 35.9375%;"><img class="icon-skull" style="top: 65.6250%; left: 35.9375%;"><img class="icon-skull" style="top: 80.2083%; left: 37.6562%;"><img class="icon-skull" style="top: 59.7917%; left: 38.2812%;"><img class="icon-skull" style="top: 45.6250%; left: 39.5312%;"><img class="icon-skull" style="top: 58.1250%; left: 41.5625%;"><img class="icon-skull" style="top: 29.7917%; left: 43.5938%;"><img class="icon-skull" style="top: 46.6667%; left: 45.1562%;"><img class="icon-skull" style="top: 40.0000%; left: 45.6250%;"><img class="icon-skull" style="top: 31.2500%; left: 50.4687%;"><img class="icon-skull" style="top: 36.4583%; left: 50.4687%;"><img class="icon-skull" style="top: 13.1250%; left: 64.3750%;"></div>`, };
+
+var skulls = {
+    downtown:      new Array(23).fill(true),
+    little_havana: new Array(21).fill(true),
+    north_beach:   new Array(26).fill(true),
+    south_beach:   new Array(13).fill(true)
+}
+
+var current_map = "little_havana";
+
+////////////////////////////////////////////////////////////////////////////////
+
+function toggleSkull(index) {
+    skulls[current_map][index] = !skulls[current_map][index];
+    var skullIcons = document.querySelectorAll(".icon-skull");
+    skullIcons[index].src = skulls[current_map][index] ? "imgs/skull_on.png" : "imgs/skull_off.png";
+    console.log(skulls[current_map]);
+    getCurrentPassword();
+}
+
+function createSkullIcons() {
+    var skullIcons = document.querySelectorAll(".icon-skull");
+    skullIcons.forEach(function (icon, index) {
+        icon.src = skulls[current_map][index] ? "imgs/skull_on.png" : "imgs/skull_off.png";
+        icon.addEventListener("click", function () { toggleSkull(index); });
+    });
+}
+
+function updateSkullIcons() {
+    var skullIcons = document.querySelectorAll(".icon-skull");
+    skullIcons.forEach(function (icon, index) {
+        icon.src = skulls[current_map][index] ? "imgs/skull_on.png" : "imgs/skull_off.png";
+    });
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+function setMapBgImage(mapName) {
+    current_map = mapName;
+    getCurrentPassword();
+
+    var container = document.getElementById("map-container");
+    container.innerHTML = html_maps[mapName];
+    createSkullIcons();
+
+    document.getElementById("page-select").value = mapName;
+    document.getElementById("map-bg").src = "imgs/" + mapName + ".jpg";
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+function compactNumberString(numberString) {
+    const byteArray = new TextEncoder().encode(numberString);
+    const base64String = btoa(String.fromCharCode(...byteArray));
+    return base64String;
+}
+
+function expandNumberString(base64String) {
+    const byteArray = new Uint8Array(atob(base64String).split("").map(char => char.charCodeAt(0)));
+    const numberString = new TextDecoder().decode(byteArray);
+    return numberString;
+}
+
+function getCurrentPassword() {
+    var pwInt = skulls[current_map].reduce((sum, x, i) => sum + x * 2 ** (i), 0);
+    var pw = compactNumberString(pwInt.toString());
+    document.getElementById("password").value = pw;
+}
+
+function setNewPassword() {
+    pwInt = expandNumberString(document.getElementById("password").value);
+    for (let i = 0; i < skulls[current_map].length; i++) {
+        skulls[current_map][i] = (pwInt & (1 << i)) != 0;
+    }
+    updateSkullIcons();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+document.addEventListener("DOMContentLoaded", function () {
+    var selectElement = document.getElementById("page-select");
+    selectElement.addEventListener("change", function () {
+        setMapBgImage(selectElement.value);
+    });
+    setMapBgImage(current_map);
+});
+
+////////////////////////////////////////////////////////////////////////////////
